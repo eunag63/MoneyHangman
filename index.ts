5cbd3e0c15bb0ba44269d49ef56e60b6c3cc0af4
eunag63 <eunag39@gmail.com>
2023-10-26 18:54:53 +0900

    ✨ express 테스트

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("hi world 🌎")
+})
+
+app.listen(port, () => {
+    console.log(`${port} porting 👀`)
+})
